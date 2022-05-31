e18fcac7acfe53043e46994b554646f70c72081f
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-05-31 13:59:08 -0600

    adds some pseudocode

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+// 1) Define the required variables used to track the state of the game
+
 
 
 
@@ -15,4 +17,21 @@
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+
+
+
+
+// 5) Handle a player clicking a square
+
+// 6) Handle a player clicking the replay button
+
+
+
+
+
+// 3) Upon loading, the app should:
+// 	3.1) Call an initialize function to initialize the state variables
+// 	3.2) Render those values to the page
+// 	3.3) Wait for the user to click a square
 
