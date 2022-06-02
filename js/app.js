8a9081da501e938868b26385db5f1a3ed977dd9f
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-06-02 15:02:19 -0600

    updates winningCombos lot of changes

--- js/app.js
+++ js/app.js
@@ -36,12 +36,12 @@ const players = {
 
 // 🧠 Each array will contain three indices of the board that make a winner if they hold the same player value (`1` or `-1`). If you are having trouble with this step, feel free to check out the `winningCombos` array in the solution code.
 
-const winningCombo = [
+const winningCombos = [
   [0, 1, 2],
-  [2, 4, 5],
+  [3, 4, 5],
   [6, 7, 8],
   [0, 3, 6],
-  [2, 4, 7],
+  [1, 4, 7],
   [3, 5, 8],
   [0, 4, 8],
   [2, 4, 6]
@@ -57,6 +57,7 @@ let playerO, playerX
 
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll('.square') // Selecting all the board's square's by class
+console.log(typeof squareEls)
 
 const messageEl = document.querySelector('#message')
 const gameBoard = document.querySelector('.board')
@@ -85,14 +86,13 @@ function init() {
 }
 
 function playerChoice(evt) {
-  console.log("playerChoice triggered")
   let idx = parseInt(evt.target.id.slice(2))
 
   if (board[idx] === null) {
-    board[idx] = turn > 0 ? 1 : -1
-    console.log(board[idx])
-    console.log(board)
 
+    board[idx] = turn // not necessary > 0 ? 1 : -1
+
+    winner = getWinner()
     // Turn over
     turn *= -1
 
@@ -102,9 +102,10 @@ function playerChoice(evt) {
 }
 
 function render() {
+  // FIX Render method here so it doesn't check winner right away?
+  
   if (winner === null) {
     // Render Message for player's turn
-    renderMessage(`${(turn > 0 ? playerX : playerO)}'s turn`)
 
     squareEls.forEach(function (e, idx) {
       if (board[idx] === 1) {
@@ -113,14 +114,12 @@ function render() {
         e.textContent = "o"
       } else if (board[idx] === null) {
         e.textContent = ""
-        console.log(`Null: ${console.dir(board[idx])}`)
-        console.log(`Element: ${console.dir(e)}`)
+        // Showing up as undefined
+        // console.log(`Null: ${console.dir(board[idx])}`)
+        // console.log(`Element: ${console.log(e.textContent)}`)
       } else {
         renderMessage(`Board square ${idx} is set to ${board[idx]}`)
       }
-      //
-      console.log(`Index: ${idx}`)
-      console.log(e)
     })
   } else {
     renderWin()
@@ -128,6 +127,7 @@ function render() {
 }
 
 function renderWin() {
+  console.log(`Render Win...Winner is ${winner}`)
   // FIXME
   // Display Replay button, maybe have it come after
   // h1 tic tac toe
@@ -143,24 +143,66 @@ function renderMessage(msg) {
 }
 
 function getWinner() {
-  // Loop through each of the winning combination arrays defined in the winningCombos array. Total up the three board positions using the three indexes in the current combo. Convert the total to an absolute value (convert any negative total to positive). If the total equals 3, we have a winner! Set the winner variable to the board's value at the index specified by the first index of that winning combination's array by returning that value.
+  // Loop through each of the winning combination arrays defined in the winningComboss array. Total up the three board positions using the three indexes in the current combo. Convert the total to an absolute value (convert any negative total to positive). If the total equals 3, we have a winner! Set the winner variable to the board's value at the index specified by the first index of that winning combination's array by returning that value.
+
+  for (let i = 0; i < winningCombos.length; i++) {
+    let sum = board[winningCombos[i][0]] + board[winningCombos[i][1]] +
+      board[winningCombos[i][2]]
+      console.log(sum)
+
+    if (sum === 3) {
+      return 1
+    } else if (sum === -3) {
+      return -1
+    }
+  }
 
 
-  for (let combo of winningCombo) {
-    let comboTotal = 0
-     combo.forEach(num => {
-       comboTotal += board[num]
-       console.log(board[num])
-       console.log(comboTotal)
-     })
-     //need to return who won if any
-     if (Mathabs(comboTotal) === 3) {
-       winner = comboTotal > 0 ? 1 : -1
-       break
-     }
-  }
-  // check for tie
-  if (winner === null) {
-    winner = board.every(square => square !== null) ? 0 : null
-  }
+  return null
+
+    // if (isBoardFull) {
+  //   return 0
+  // }
+
+  // for (let combo of winningCombos) {
+  //   let comboTotal = 0
+  //   combo.forEach(num => {
+  //     console.log(`Current Combo Num: ${num}`)
+  //     console.log(`board num: ${board[num]}`)
+  //     comboTotal += board[num]
+  //     console.log(board[num])
+  //     console.log(comboTotal)
+  //   })
+  //   console.log(`comboTotal: ${comboTotal}`)
+  //   //need to return who won if any
+  //   if (Math.abs(comboTotal) === 3) {
+  //     return comboTotal > 0 ? 1 : -1
+  //   }
+  // }
+  // // check for tie
+  // if (winner === null) {
+  //   winner = isBoardFull() ? 0 : null
+
+  //   // winner = board.every(square => square !== null) ? 0 : null
+
+
+  //   // let temp
+  //   // temp  = board.every(function(square) {
+  //   //   return square !== null
+  //   // })
+  //   // if (temp) {
+  //   //   winner = 0 // This is a tie
+  //   // } else {
+  //   //   winner = null
+  //   // }
+
+}
+
+function isBoardFull() {
+  board.forEach(function (square) {
+    if (square === null) {
+      return false
+    }
+  })
+  return true
 }
