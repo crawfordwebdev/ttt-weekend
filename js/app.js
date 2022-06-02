cb810186a1ea5aa9c6fe12b2af2d56eab43a7569
Michael Crawford <webdevcrawford@gmail.com>
2022-06-01 22:13:58 -0600

    adds reset button adds some structure for winning logic

--- js/app.js
+++ js/app.js
@@ -30,6 +30,49 @@ const players = {
 };
 
 
+// ## Step 5 - Define the required constants
+
+// a) In a constant called `winningCombos` define the eight possible winning combinations as an array of arrays.
+
+// 🧠 Each array will contain three indices of the board that make a winner if they hold the same player value (`1` or `-1`). If you are having trouble with this step, feel free to check out the `winningCombos` array in the solution code.
+
+
+const winningCombos = [
+  [
+    1, 1, 1, 
+    0, 0, 0,
+    0, 0, 0
+  ],[
+    0, 0, 0, 
+    1, 1, 1,
+    0, 0, 0
+  ],[
+    0, 0, 0, 
+    0, 0, 0,
+    1, 1, 1
+  ],[
+    1, 0, 0, 
+    1, 0, 0,
+    1, 0, 0
+  ],[
+    0, 1, 0, 
+    0, 1, 0,
+    0, 1, 0
+  ],[
+    0, 0, 1, 
+    0, 0, 1,
+    0, 0, 1
+  ],[
+    1, 0, 0, 
+    0, 1, 0,
+    0, 0, 1
+  ],[
+    0, 0, 1, 
+    0, 1, 0,
+    1, 0, 0
+  ],
+]
+
 /*---------------------------- Variables (state) ----------------------------*/
 // 1) Define the required variables used to track the state of the game
 
@@ -44,8 +87,11 @@ const squareEls = document.querySelectorAll('.square') // Selecting all the boar
 const messageEl = document.querySelector('#message')
 const gameBoard = document.querySelector('.board')
 
+const resetBtnEl = document.querySelector('#reset-button')
+
 /*----------------------------- Event Listeners -----------------------------*/
 gameBoard.addEventListener('click', playerChoice)
+resetBtnEl.addEventListener('click', init)
 
 
 /*-------------------------------- Functions --------------------------------*/
@@ -67,7 +113,7 @@ function init() {
 function playerChoice(evt) {
   console.log("playerChoice triggered")
   let idx = parseInt(evt.target.id.slice(2))
-  // is it blank?
+
   if (board[idx] === null) {
     board[idx] = turn > 0 ? 1 : -1
     console.log(board[idx])
@@ -92,7 +138,7 @@ function render() {
       } else if (board[idx] === -1) {
         e.textContent = "o"
       } else if (board[idx] === null) {
-        // e.textContent = ""
+        e.textContent = ""
         console.log(`Null: ${console.dir(board[idx])}`)
         console.log(`Element: ${console.dir(e)}`)
       } else {
@@ -120,3 +166,53 @@ function renderWin() {
 function renderMessage(msg) {
   messageEl.textContent = msg
 }
+
+function checkForWinner() {
+
+  // new array copy of board to see if one player has one and then another array to see if other has one
+  // should be able to compare this array to each in the winningCombos array to see if any match
+
+  /*  
+    General Notes
+    Player X : 1
+    Player O : -1
+
+    Change Turns
+    Turn *= -1
+
+    winner
+    Player O : -1
+    Tie : 0
+    Player X : 1
+*/
+
+  // Check if board is filled
+  if (board.every(e => e !== null)) {
+    
+
+  }
+
+
+}
+
+function equalArrayCompare(a, b) {
+  if (a.length !== b.length) {
+    return false
+  } else  {
+    a.forEach(function(el, idx) {
+      console.log(`el: ${el}`)
+      console.log(`b[idx] ${b[idx]}`)
+      if (el != b[idx]) {
+        return false
+      }
+    })
+  }
+  return true
+}
+
+
+let testArray = [
+  null, null, null,
+  null, 1, null,
+  null, null, null]
+console.log(`Comparing Array: ${equalArrayCompare(testArray,board)}`)
