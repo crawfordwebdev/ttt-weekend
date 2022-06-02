4d7748e664b5670771aeccc424cfb0ead5d774e7
Michael Crawford <webdevcrawford@gmail.com>
2022-06-01 22:26:20 -0600

    fix for winningCombo 2D Array

--- js/app.js
+++ js/app.js
@@ -36,41 +36,15 @@ const players = {
 
 // 🧠 Each array will contain three indices of the board that make a winner if they hold the same player value (`1` or `-1`). If you are having trouble with this step, feel free to check out the `winningCombos` array in the solution code.
 
-
-const winningCombos = [
-  [
-    1, 1, 1, 
-    0, 0, 0,
-    0, 0, 0
-  ],[
-    0, 0, 0, 
-    1, 1, 1,
-    0, 0, 0
-  ],[
-    0, 0, 0, 
-    0, 0, 0,
-    1, 1, 1
-  ],[
-    1, 0, 0, 
-    1, 0, 0,
-    1, 0, 0
-  ],[
-    0, 1, 0, 
-    0, 1, 0,
-    0, 1, 0
-  ],[
-    0, 0, 1, 
-    0, 0, 1,
-    0, 0, 1
-  ],[
-    1, 0, 0, 
-    0, 1, 0,
-    0, 0, 1
-  ],[
-    0, 0, 1, 
-    0, 1, 0,
-    1, 0, 0
-  ],
+const winningCombo = [
+  [0, 1, 2],
+  [2, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [2, 4, 7],
+  [3, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ]
 
 /*---------------------------- Variables (state) ----------------------------*/
